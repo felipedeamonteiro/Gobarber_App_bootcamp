2cc3fabc5391648ac1bd08040d1485cd97a62044
Felipe Monteiro <felipedeamonteiro@gmail.com>
2020-09-16 00:25:31 -0300

    Add pages, routes and Dashboar header

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,14 +1,56 @@
-import React from 'react';
-import { View, Button } from 'react-native';
+/* eslint-disable camelcase */
+import React, { useCallback, useEffect, useState } from 'react';
+
+import { useNavigation } from '@react-navigation/native';
 import { useAuth } from '../../hooks/auth';
+import api from '../../services/api';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  UserName,
+  ProfileButton,
+  UserAvatar,
+} from './styles';
+
+interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  const { signOut } = useAuth();
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  const { signOut, user } = useAuth();
+  const { navigate } = useNavigation();
+
+  useEffect(() => {
+    api.get('providers').then(response => {
+      setProviders(response.data);
+    });
+  }, []);
+
+  const navigateToProfile = useCallback(() => {
+    navigate('Profile');
+  }, [navigate]);
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center' }}>
-      <Button title="Sair" onPress={signOut} />
-    </View>
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo(a),
+{' '}
+{'\n'}
+          <UserName>{user.name}</UserName>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+    </Container>
   );
 };
 
